bd0ec31d0d91b6e23a2b865ec376ac30bdc7f961
Shaun Dern <shaun@substantial.com>
2016-05-23 14:47:31 -0700

    Break mix release
    Move a defined module outside of our app and into a lib dependency

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExrmMovingModuleRepro.Mixfile do
 
   def project do
     [app: :exrm_moving_module_repro,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -37,7 +37,7 @@ defmodule ExrmMovingModuleRepro.Mixfile do
     [
       {:poison, "~> 1.0"},
       {:exrm, "1.0.4"},
-      {:monetized, "0.4.0"},
+      {:monetized, github: "substantial/monetized", ref: "multi-branch-fix"},
     ]
   end
 end
